f41cd5da8f58f03f37dc2338b736245738a9c3e2
Niladri Chatterjee <code.niladri@gmail.com>
2025-01-13 20:34:13 +0530

    Upload draft

--- backend/src/index.js
+++ backend/src/index.js
@@ -19,24 +19,24 @@ const __dirname = path.resolve();
 app.use(express.json());
 app.use(cookieParser());
 app.use(
-  cors({
-    origin: "http://localhost:5173",
-    credentials: true,
-  })
+	cors({
+		origin: "http://localhost:5173",
+		credentials: true,
+	})
 );
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+	app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
-  });
+	app.get("*", (req, res) => {
+		res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+	});
 }
 
 server.listen(PORT, () => {
-  console.log("server is running on PORT:" + PORT);
-  connectDB();
+	console.log("server is running on PORT:" + PORT);
+	connectDB();
 });
